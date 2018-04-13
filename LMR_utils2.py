8cd8c31754918e3206ab87d4abade9287447a3cb
Andre Perkins <frodre@gmail.com>
2018-04-13 07:09:40 -0700

    Transition to work

--- LMR_utils2.py
+++ LMR_utils2.py
@@ -27,6 +27,7 @@ import collections
 import copy
 import tables as tb
 import ESMF
+import warnings
 from time import time
 from os.path import join
 from math import radians, cos, sin, asin, sqrt
@@ -1263,56 +1264,130 @@ def generate_latlon(nlats, nlons, lat_bnd=(-90, 90), lon_bnd=(0, 360),
     return lat_center_2d, lon_center_2d, lat_corner, lon_corner
 
 
-def calculate_latlon_bnds(lats, lons):
+def calculate_latlon_bnds(lats, lons, lat_ax=0):
     """
-    Calculate the bounds for regularly gridded lats and lons.
+    Calculate the bounds for gridded lats and lons.
 
     Parameters
     ----------
     lats: ndarray
-        Regularly spaced latitudes.  Must be 1-dimensional and monotonically 
-        increase with index.
+        Latitude array. Can be either 1D or 2D.
     lons:  ndarray
-        Regularly spaced longitudes.  Must be 1-dimensional and monotonically 
-        increase with index.
+        Longitude array.  Can be either 1D or 2D.
 
     Returns
     -------
     lat_bnds:
-        Array of latitude boundaries for each input latitude of length 
-        len(lats)+1.
+        Array of latitude boundaries for each input latitude.
     lon_bnds:
-        Array of longitude boundaries for each input longitude of length
-        len(lons)+1.
+        Array of longitude boundaries for each input longitude.
+
+    Notes
+    -----
+    This function was originally built with regularly gridded coordinate
+    dimensions in mind.  It was extended to be useful for conservative
+    regridding of irregular grids, but only tested on Rotated Pole grids.
+    When using funky grids it is best to make sure output makes sense.
 
     """
+    if not 1 <= lats.ndim <= 2 or not 1 <= lons.ndim <= 2:
+        raise ValueError('Input lats and lons must be 1D or 2D')
+    if lats.ndim != lons.ndim or lats.shape != lons.shape:
+        raise ValueError('Input lats and lons must have the same dimensions')
+
     if lats.ndim == 2:
-        lat_dim = int(not lon_dim)
-    elif lats.ndim != lons.ndim:
-        raise ValueError('Input lats and lons must have same number of '
-                         'dimensions.')
-    elif lats.ndim > 2 or lons.ndim > 2:
-        raise ValueError('Expected 1D or 2D array for input lats and lons.')
+        lon_ax = int(np.logical_not(lat_ax))
+        bnd_2d = True
+    else:
+        lat_ax = lon_ax = 0
+        bnd_2d = False
 
-    # Check that inputs are regularly spaced
-    lat_space = np.diff(lats)
-    lon_space = np.diff(lons)
+    dim_order = sorted([lat_ax, lon_ax])
 
-    lat_bnds = np.zeros(len(lats) + 1)
-    lon_bnds = np.zeros(len(lons) + 1)
+    lat_space = np.diff(lats, axis=lat_ax)
+    lon_space = np.diff(lons, axis=lon_ax)
 
-    lat_bnds[1:-1] = lats[:-1] + lat_space / 2.
-    lat_bnds[0] = lats[0] - lat_space[0] / 2.
-    lat_bnds[-1] = lats[-1] + lat_space[-1] / 2.
+    nlat = lats.shape[lat_ax]
+    nlon = lons.shape[lon_ax]
 
-    if lat_bnds[0] < -90:
-        lat_bnds[0] = -90.
-    if lat_bnds[-1] > 90:
-        lat_bnds[-1] = 90.
+    bnd_shp = [dim_len + 1 for dim_len in lats.shape]
 
-    lon_bnds[1:-1] = lons[:-1] + lon_space / 2.
-    lon_bnds[0] = lons[0] - lon_space[0] / 2.
-    lon_bnds[-1] = lons[-1] + lon_space[-1] / 2.
+    lat_bnds = np.zeros(bnd_shp)
+    lon_bnds = np.zeros_like(lat_bnds)
+
+    # Handle cyclic point if necessary
+    if bnd_2d:
+        if np.any(lon_space > 300):
+            i_idx, j_idx = np.where(lon_space > 300)
+            lon_space[i_idx, j_idx] = lon_space[i_idx, j_idx + 1]
+
+        if np.any(lon_space < -300):
+            i_idx, j_idx = np.where(lon_space < -300)
+            lon_space[i_idx, j_idx] = lon_space[i_idx, j_idx - 1]
+    else:
+        if np.any(lon_space > 300):
+            i_idx, = np.where(lon_space > 300)
+            lon_space[i_idx] = lon_space[i_idx + 1]
+
+        if np.any(lon_space < -300):
+            i_idx, = np.where(lon_space < -300)
+            lon_space[i_idx] = lon_space[i_idx - 1]
+
+    # Handle out of bounds latitudes
+    lat_space[lat_space > 90] = 90
+    lat_space[lat_space < -90] = -90
+
+    lon_sl = slice(0, nlon-1)
+    lat_sl = slice(0, nlat-1)
+    all_but_last = slice(0, -1)
+    last_two = slice(-2)
+    all_vals = slice(None)
+
+    # TODO: Not an elegant solution for variable dimension order but I think it
+    # works...
+    if bnd_2d:
+        # Create slices to be used for general dimension order
+        bnd_slice = (all_but_last, lon_sl)
+        coord_slice = (all_vals, all_but_last)
+        bnd_end_slice = (all_but_last, last_two)
+        coord_end_slice = (all_vals, last_two)
+        diff_end_slice = (all_vals, last_two)
+        cyclic_bnd_dst = (-1, all_vals)
+        cyclic_bnd_src = (-2, all_vals)
+
+        # If lon changes over first dimension we want to reverse the slice
+        # tuples
+        if lon_ax == 0:
+            rev = -1
+        else:
+            rev = 1
+
+        lon_bnds[bnd_slice[::rev]] = lons[coord_slice[::rev]] - lon_space / 2
+        lon_bnds[bnd_end_slice[::rev]] = (lons[coord_end_slice[::rev]] +
+                                          lon_space[diff_end_slice[::rev]] / 2)
+        lon_bnds[cyclic_bnd_dst[::rev]] = lon_bnds[cyclic_bnd_src[::rev]]
+
+        # Adjust the bnd slice for latitude dimension
+        bnd_slice = (all_but_last, lat_sl)
+
+        # If lat changes over first dimension we want to reverse the slice
+        # tuples
+        if lat_ax == 0:
+            rev = -1
+        else:
+            rev = 1
+
+        lat_bnds[bnd_slice[::rev]] = lats[coord_slice[::rev]] - lat_space / 2
+        lat_bnds[bnd_end_slice[::rev]] = (lats[coord_end_slice[::rev]] +
+                                          lat_space[diff_end_slice[::rev]] / 2)
+        lat_bnds[cyclic_bnd_dst[::rev]] = lat_bnds[cyclic_bnd_src[::rev]]
+
+    else:
+        lon_bnds[lon_sl] = lons[all_but_last] - lon_space / 2
+        lon_bnds[last_two] = lons[last_two] + lon_space[last_two] / 2
+
+        lat_bnds[lat_sl] = lats[all_but_last] - lat_space / 2
+        lat_bnds[last_two] = lats[last_two] + lat_space[last_two] / 2
 
     return lat_bnds, lon_bnds
 
