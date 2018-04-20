f8ac73d60efc66e962b0f10a8db10c8aeb13d92e
Andre Perkins <frodre@gmail.com>
2018-04-20 08:59:50 -0700

    Fix LIM fit_noise parameter name in init.

--- LMR_forecaster.py
+++ LMR_forecaster.py
@@ -101,7 +101,7 @@ class LIMForecaster(BaseForecaster):
             fit_noise = False
 
         self.lim = LIM.LIM(calib_state, nelem_in_tau1=nelem_in_yr,
-                           fit_nosie=fit_noise)
+                           fit_noise=fit_noise)
         self.var_order = var_order
         self.var_eofs = calib_eofs
         self.fcast_state_bnds = fcast_state_bnds
